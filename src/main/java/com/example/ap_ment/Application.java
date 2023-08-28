8ae207925e184e25892390ae40283e11ccfde76f
Andriy <sandriy666@gmail.com>
2023-08-28 18:37:50 +0300

    added start files

---
+++ src/main/java/com/example/ap_ment/Application.java
@@ -0,0 +1,13 @@
+package com.example.ap_ment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
